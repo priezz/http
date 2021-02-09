741f39085a88c117d51cd616899800c2178b11df
Alexandr Priezzhev <a@priezz.net>
2021-02-09 01:21:43 +0100

    Upgrade dependencies

--- lib/src/node_client.dart
+++ lib/src/node_client.dart
@@ -6,4 +6,4 @@ export 'package:node_http/node_http.dart' show NodeClient;
 /// Create an [NodeClient].
 ///
 /// Used from conditional imports, matches the definition in `client_stub.dart`.
-BaseClient createClient() => NodeClient() as BaseClient;
+BaseClient createClient() => NodeClient();
